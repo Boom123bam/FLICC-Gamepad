766d417ce4f8e82ee22fc879409decb83df970bb
tylerfowle <tylerfowle@gmail.com>
2021-01-13 15:07:06 -0600

    formatting

--- stuff/flicc/keymaps/default/keymap.c
+++ stuff/flicc/keymaps/default/keymap.c
@@ -21,13 +21,12 @@
 //CHANGE THIS
 char arrow_keys[4] = {KC_UP, KC_LEFT, KC_DOWN, KC_RIGHT}; // up, left, down, right
 
-
 static uint8_t joystickMode = 0;
 static uint8_t joystick_modes = 2; // amount of modes
-static int actuation = 256; // actuation point for arrows (0-511)
+static int actuation = 256;        // actuation point for arrows (0-511)
 
 enum my_keycodes {
-  JSMODE = SAFE_RANGE,
+    JSMODE = SAFE_RANGE,
 };
 
 bool arrows[4];
@@ -46,6 +45,7 @@ void keyboard_pre_init_user(void) {
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
     [0] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    MO(1),
         KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
@@ -62,11 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,   KC_TRNS,   KC_TRNS,   JSMODE,             KC_TRNS
     ),
     
-
 };
-
-
-
 
 //joystick config
 joystick_config_t joystick_axes[JOYSTICK_AXES_COUNT] = {
@@ -76,66 +72,63 @@ joystick_config_t joystick_axes[JOYSTICK_AXES_COUNT] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
-//#ifdef CONSOLE_ENABLE
-//    uprintf("kc:%u x:%u y:%u mode:%u\n", analogReadPin(F5), analogReadPin(F4), keycode, joystickMode, record->event.pressed);
-//#endif 
-    switch(keycode) {
+    //#ifdef CONSOLE_ENABLE
+    //    uprintf("kc:%u x:%u y:%u mode:%u\n", analogReadPin(F5), analogReadPin(F4), keycode, joystickMode, record->event.pressed);
+    //#endif
+    switch (keycode)
+    {
     case JSMODE:
         if (record->event.pressed) {
             joystickMode++;
-        	if (joystickMode == joystick_modes){
-        		joystickMode = 0;
+            if (joystickMode == joystick_modes) {
+                joystickMode = 0;
             }
         }
     }
     return true;
 }
 
-void joystick_task(){
+void joystick_task() {
 
-	switch (joystickMode) {
-	case 0: // gamepad
-		joystick_status.axes[0] = -(analogReadPin(F5)/4 - 127);
-		joystick_status.axes[1] = analogReadPin(F4)/4 - 128;
-		joystick_status.status |= JS_UPDATED;
+    switch (joystickMode) {
+    case 0: // gamepad
+        joystick_status.axes[0] = -(analogReadPin(F5) / 4 - 127);
+        joystick_status.axes[1] = analogReadPin(F4) / 4 - 128;
+        joystick_status.status |= JS_UPDATED;
         send_joystick_packet(&joystick_status);
-		break;
+        break;
     case 1: // arrows
-        if (!arrows[0] && analogReadPin(F5) - 512 > actuation){
+        if (!arrows[0] && analogReadPin(F5) - 512 > actuation) {
             arrows[0] = true;
             register_code16(arrow_keys[1]);
-        }
-        else if (arrows[0] &&  analogReadPin(F5) - 512 < actuation){
+        } else if (arrows[0] && analogReadPin(F5) - 512 < actuation) {
             arrows[0] = false;
             unregister_code16(arrow_keys[1]);
         }
-        if (!arrows[1] && analogReadPin(F5) - 512 < -actuation){
+        if (!arrows[1] && analogReadPin(F5) - 512 < -actuation) {
             arrows[1] = true;
             register_code16(arrow_keys[3]);
-        }
-        else if (arrows[1] && analogReadPin(F5) - 512 > -actuation){
+        } else if (arrows[1] && analogReadPin(F5) - 512 > -actuation) {
             arrows[1] = false;
             unregister_code16(arrow_keys[3]);
         }
-        if (!arrows[2] && analogReadPin(F4) - 512 > actuation){
+        if (!arrows[2] && analogReadPin(F4) - 512 > actuation) {
             arrows[2] = true;
             register_code16(arrow_keys[2]);
-        }
-        else if (arrows[2] &&  analogReadPin(F4) - 512 < actuation){
+        } else if (arrows[2] && analogReadPin(F4) - 512 < actuation) {
             arrows[2] = false;
             unregister_code16(arrow_keys[2]);
         }
-        if (!arrows[3] && analogReadPin(F4) - 512 < -actuation){
+        if (!arrows[3] && analogReadPin(F4) - 512 < -actuation) {
             arrows[3] = true;
             register_code16(arrow_keys[0]);
-        }
-        else if (arrows[3] && analogReadPin(F4) - 512 > -actuation){
+        } else if (arrows[3] && analogReadPin(F4) - 512 > -actuation) {
             arrows[3] = false;
             unregister_code16(arrow_keys[0]);
         }
         break;
 
-    /*case 2: // mouse
+        /*case 2: // mouse
 
         report_mouse_t currentReport = pointint_device_get_report()
         currentReport.x = (analogReadPin(F5)-512)/4;
@@ -143,7 +136,7 @@ void joystick_task(){
         pointint_device_set_report(currentReport);
         poining_device_send();
         break;*/
-	}
+    }
 }
 
 // Automatically sets leds based on the layer
@@ -154,7 +147,7 @@ uint32_t layer_state_set_user(uint32_t state) {
     writePin(LED2, 0);
     writePin(LED3, 0);
 
-    switch(biton32(state)) {
+    switch (biton32(state)) {
         case 0:
             // no lights
             break;
@@ -191,7 +184,6 @@ uint32_t layer_state_set_user(uint32_t state) {
             writePin(LED2, 50);
             writePin(LED3, 50);
             break;
-
     }
     return state;
 }
